864ed793a310d9858a19b7e4ea8c35eb55de6c35
Tiziano <totoyabue.07@gmail.com>
2023-05-10 15:38:00 -0300

    ejercicio 1 completo

---
+++ ejercicio1.c
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+<ol>
+<li>Chacarita</li>
+<li>Argentinos Juniors</li>
+<li>Newells</li>
+</ol>
+</body>
+</html>
